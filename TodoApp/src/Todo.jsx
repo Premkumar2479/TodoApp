8aea2afbed1d948a16c46472dc165b977137a204
Premkumar2479 <pk2335301@gmail.com>
2026-05-17 03:17:44 +0530

    some updates

--- TodoApp/src/Todo.jsx
+++ TodoApp/src/Todo.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 function Todo() {
+
     const [newTodo, setNewTodo] = useState('');
     const [todos, setTodos] = useState([]);
 
@@ -8,9 +9,16 @@ function Todo() {
         e.preventDefault();
 
         if (newTodo.trim()) {
+
             setTodos([
                 ...todos,
-                { text: newTodo, completed: false }
+                {
+                    text:
+                        newTodo.charAt(0).toUpperCase() +
+                        newTodo.slice(1),
+
+                    completed: false
+                }
             ]);
 
             setNewTodo('');
@@ -18,36 +26,59 @@ function Todo() {
     };
 
     const handleDelete = (index) => {
-        const updatedTodos = todos.filter((_, i) => i !== index);
+
+        const updatedTodos = todos.filter(
+            (_, i) => i !== index
+        );
+
         setTodos(updatedTodos);
     };
 
     return (
-        <div>
+        <div className="container">
+
             <h1>Todo App</h1>
 
             <form onSubmit={handleSubmit}>
+
                 <input
                     type="text"
                     placeholder="Add new todo"
                     value={newTodo}
-                    onChange={(e) => setNewTodo(e.target.value)}
+                    onChange={(e) =>
+                        setNewTodo(e.target.value)
+                    }
                 />
 
-                <button type="submit">Add Todo</button>
+                <button type="submit">
+                    Add Todo
+                </button>
+
             </form>
 
             <ul>
-                {todos.map((todo, index) => (
-                    <li key={index}>
-                        <span>{todo.text}</span>
 
-                        <button onClick={() => handleDelete(index)}>
+                {todos.map((todo, index) => (
+
+                    <li key={index}>
+
+                        <span>
+                            {todo.text}
+                        </span>
+
+                        <button
+                            onClick={() =>
+                                handleDelete(index)
+                            }
+                        >
                             Delete
                         </button>
+
                     </li>
                 ))}
+
             </ul>
+
         </div>
     );
 }
